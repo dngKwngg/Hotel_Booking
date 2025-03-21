d1722dffc91fa73621755a0b6ac8f33f4150d73f
Nguyen Dang Quang <quangnguyenvk@gmail.com>
2025-03-21 15:20:04 +0700

    Update schema for PostgreSQL instead of MySQL

--- schema.sql
+++ schema.sql
@@ -1,5 +1,4 @@
 -- CREATE DATABASE booking_management;
-
 DROP TABLE IF EXISTS bookings;
 DROP TABLE IF EXISTS hotel_amenities;
 DROP TABLE IF EXISTS hotel_rooms;
@@ -9,93 +8,62 @@ DROP TABLE IF EXISTS room_types;
 DROP TABLE IF EXISTS provinces;
 DROP TABLE IF EXISTS users;
 
-
 CREATE TABLE users (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	email VARCHAR(255),
-    username VARCHAR(255),
-    password VARCHAR(255)
+                       id SERIAL PRIMARY KEY,
+                       email VARCHAR(255),
+                       username VARCHAR(255),
+                       password VARCHAR(255)
 );
-
-
-
-
 
 CREATE TABLE provinces (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(255)
+                           id SERIAL PRIMARY KEY,
+                           name VARCHAR(255)
 );
-
-
-
-
-
-
 
 CREATE TABLE hotels (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(255),
-    province_id INT,
-    FOREIGN KEY (province_id) REFERENCES provinces(id)
+                        id SERIAL PRIMARY KEY,
+                        name VARCHAR(255),
+                        province_id INT,
+                        FOREIGN KEY (province_id) REFERENCES provinces(id) ON DELETE CASCADE
 );
-
-
-
-
-
 
 CREATE TABLE room_types (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(255),
-    price INT
+                            id SERIAL PRIMARY KEY,
+                            name VARCHAR(255),
+                            price INT
 );
-
-
-
-
-
 
 CREATE TABLE hotel_rooms (
-	hotel_id INT ,
-    room_type_id INT,
-    number_rooms INT,
-    PRIMARY KEY (hotel_id, room_type_id),
-	FOREIGN KEY (hotel_id) REFERENCES hotels(id),
-	FOREIGN KEY (room_type_id) REFERENCES room_types(id)
+                             hotel_id INT,
+                             room_type_id INT,
+                             number_rooms INT,
+                             PRIMARY KEY (hotel_id, room_type_id),
+                             FOREIGN KEY (hotel_id) REFERENCES hotels(id) ON DELETE CASCADE,
+                             FOREIGN KEY (room_type_id) REFERENCES room_types(id) ON DELETE CASCADE
 );
-
-
-
-
-
 
 CREATE TABLE amenities (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(255)
+                           id SERIAL PRIMARY KEY,
+                           name VARCHAR(255)
 );
-
-
 
 CREATE TABLE hotel_amenities (
-	hotel_id INT ,
-    amenity_id INT,
-    PRIMARY KEY (hotel_id, amenity_id),
-    FOREIGN KEY (hotel_id) REFERENCES hotels(id),
-    FOREIGN KEY (amenity_id) REFERENCES amenities(id)
+                                 hotel_id INT,
+                                 amenity_id INT,
+                                 PRIMARY KEY (hotel_id, amenity_id),
+                                 FOREIGN KEY (hotel_id) REFERENCES hotels(id) ON DELETE CASCADE,
+                                 FOREIGN KEY (amenity_id) REFERENCES amenities(id) ON DELETE CASCADE
 );
 
-
-
-
 CREATE TABLE bookings (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	user_id INT UNIQUE,
-    hotel_id INT UNIQUE,
-    checkin_date INT,
-    checkout_date INT,
-	total_fare INT,
-    booking_date INT,
-    status_payment VARCHAR(255),
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (hotel_id) REFERENCES hotels(id)
+                          id SERIAL PRIMARY KEY,
+                          user_id INT,
+                          hotel_id INT,
+                          checkin_date DATE,
+                          checkout_date DATE,
+                          total_fare INT,
+                          booking_date DATE,
+                          status_payment VARCHAR(255),
+                          FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+                          FOREIGN KEY (hotel_id) REFERENCES hotels(id) ON DELETE CASCADE,
 );
