bd79e1758d44524fb96ee5f63203475de6218cc9
Nguyen Dang Quang <quangnguyenvk@gmail.com>
2025-03-21 16:17:57 +0700

    Add missing field in schema

--- schema.sql
+++ schema.sql
@@ -14,6 +14,8 @@ CREATE TABLE users (
                        email VARCHAR(255) UNIQUE NOT NULL,
                        username VARCHAR(255) UNIQUE NOT NULL,
                        password VARCHAR(255) NOT NULL,
+                       first_name VARCHAR(255) NOT NULL,
+                        last_name VARCHAR(255) NOT NULL,
                        role_id INT NOT NULL,  -- Foreign key to roles
                        FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE
 );
